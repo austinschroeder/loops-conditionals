23e2521ba3fc6452fec850443b2ce1f70a1a009f
austinschroeder <austinsoundy@gmail.com>
2021-04-07 21:08:21 -0700

    Commit 6 - Yell at the Ninja Turtles answered

--- app.js
+++ app.js
@@ -42,39 +42,39 @@
 //     }
 // }
 
-////// Use the following arrays to answer the questions below (name, age, hometown):
-const kenny = ["Kenny", 1000, "Austin"];
-const jimHaff = ["Jim H", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const jimClark = ["Jim C", 186, "LA"];
-const ryan = ["Ryan", 65, "Denver"];
-////// Jim Clark decides that Kenny can't be named "Kenny" anymore. Remove "Kenny" from the kenny array and replace it with "Gameboy".
-kenny.splice(0, 1, 'Gameboy');
-console.log(kenny);
-////// Jim Clark just had his birthday; change jimClark's array to reflect him being a year older. Don't just hard code 187--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
-jimClark.splice(1, 1, jimClark[1] + 1);
-console.log(jimClark);
-////// Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
-ryan.splice(2, 1, "Gotham City");
-console.log(ryan);
-////// Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago".
-reuben.pop()
-reuben.push('Chicago');
-//console.log(reuben);
-////// Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array.
-jimHaff.pop()
-jimHaff.push('Boston', 'Atlanta', 'Nashville');
-console.log(jimHaff);
-////// Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim Haff is now, remove it from the array using
-jimHaff.splice(2, 1);
-console.log(jimHaff);
+// //// Use the following arrays to answer the questions below (name, age, hometown):
+// const kenny = ["Kenny", 1000, "Austin"];
+// const jimHaff = ["Jim H", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const jimClark = ["Jim C", 186, "LA"];
+// const ryan = ["Ryan", 65, "Denver"];
+// ////// Jim Clark decides that Kenny can't be named "Kenny" anymore. Remove "Kenny" from the kenny array and replace it with "Gameboy".
+// kenny.splice(0, 1, 'Gameboy');
+// console.log(kenny);
+// ////// Jim Clark just had his birthday; change jimClark's array to reflect him being a year older. Don't just hard code 187--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
+// jimClark.splice(1, 1, jimClark[1] + 1);
+// console.log(jimClark);
+// ////// Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+// ryan.splice(2, 1, "Gotham City");
+// console.log(ryan);
+// ////// Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago".
+// reuben.pop()
+// reuben.push('Chicago');
+// //console.log(reuben);
+// ////// Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array.
+// jimHaff.pop()
+// jimHaff.push('Boston', 'Atlanta', 'Nashville');
+// console.log(jimHaff);
+// ////// Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim Haff is now, remove it from the array using
+// jimHaff.splice(2, 1);
+// console.log(jimHaff);
 
 ////// Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
 const turtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
 ////// Use a for loop to call .toUpperCase() on each of them and print out the result.
 for (let i=0; i <= 3; i++) {
- //   console.log(turtles[i].toUpperCase());
-    //console.log(turtles[i].split(''));
+    console.log(turtles[i].toUpperCase());
+    console.log(turtles[i].split(''));
 }
 ////// Bonus: Modify the answer you just wrote. Instead of all letters being uppercase, make the letters alternate back and forth between uppercase and lowercase.
 
