4cb79a86ca0c2222d2927712ee99954d95415917
austinschroeder <austinsoundy@gmail.com>
2021-04-13 17:06:24 -0700

    FINAL COMMIT

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 ////// Write a for loop that will log only the even numbers in 0 through 200.
-// for (let i = 1; i <= 20; i++) {
-//     console.log(i);
-// }
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
 
 ////// Write a for loop that will log only the even numbers in 0 through 200
 
-// for (let i = 0; i <= 200; i += 2) {
-//     console.log(i);
-// }
+for (let i = 0; i <= 200; i += 2) {
+    console.log(i);
+}
 
 ////// Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
 //////For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
-// const kitty = 'Love me, pet me! HSSSSSS!';
-// const kittyArray = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
-// for (let i = 1; i <= 20; i++) {
-//     if (i % 2 === 1) {
-//     console.log(kitty);
-//     } else {
-//         const randomIndex = Math.floor(Math.random() * 3)
-//     console.log(kittyArray[randomIndex]);
-//     }
-// }
+const kitty = 'Love me, pet me! HSSSSSS!';
+const kittyArray = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 1) {
+    console.log(kitty);
+    } else {
+        const randomIndex = Math.floor(Math.random() * 3)
+    console.log(kittyArray[randomIndex]);
+    }
+}
 
 
 ////// Write a javascript application that logs all numbers from 1 - 100 AND:
@@ -30,32 +30,32 @@
 ////// If a number is divisible by 5 log "Buzz" instead of the number.
 
 ////// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number, "Fizz", or "Buzz".
-// for (let i = 0; i <= 100; i++) {
-//     if ((i % 3 === 0) && (i % 5 === 0)) {
-//         console.log('FizzBuzz');
-//     } else if (i % 5 === 0) {
-//         console.log('Buzz');
-//     } else if (i % 3 === 0) {
-//         console.log('Fizz');
-//     } else {
-//         console.log(i);
-//     }
-// }
+for (let i = 0; i <= 100; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log('FizzBuzz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else {
+        console.log(i);
+    }
+}
 
-// for (let i = 1; i <= 100; i++) {
-//   if (i % 3 === 0) {
-//       console.log(i + " Fizz");
-//   }
-//   if (i % 5 === 0) {
-//       console.log(i + " Buzz");
-//   }
-//   if ((i % 3 === 0) && (i % 5 === 0)) {
-//       console.log(i + " FizzBuzz");
-//   }
-//   if ((i % 3 !== 0) || (i % 5 !== 0)) {
-//       console.log(i);
-//   }
-// };
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+      console.log(i + " Fizz");
+  }
+  if (i % 5 === 0) {
+      console.log(i + " Buzz");
+  }
+  if ((i % 3 === 0) && (i % 5 === 0)) {
+      console.log(i + " FizzBuzz");
+  }
+  if ((i % 3 !== 0) || (i % 5 !== 0)) {
+      console.log(i);
+  }
+};
 
 // //// Use the following arrays to answer the questions below (name, age, hometown):
 // const kenny = ["Kenny", 1000, "Austin"];
@@ -81,16 +81,16 @@
 // jimHaff.push('Boston', 'Atlanta', 'Nashville');
 // console.log(jimHaff); // splice works, too
 // ////// Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim Haff is now, remove it from the array using
-// jimHaff.splice(2, 1);
-// console.log(jimHaff);
+jimHaff.splice(2, 1);
+console.log(jimHaff);
 
 ////// Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
 const turtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
 ////// Use a for loop to call .toUpperCase() on each of them and print out the result.
-// for (let i=0; i <= 3; i++) {
-//     console.log(turtles[i].toUpperCase());
-//     console.log(turtles[i].split(''));
-// }
+for (let i=0; i <= 3; i++) {
+    console.log(turtles[i].toUpperCase());
+    console.log(turtles[i].split(''));
+}
 ////// Bonus: Modify the answer you just wrote. Instead of all letters being uppercase, make the letters alternate back and forth between uppercase and lowercase.
 
 const kristynsCloset = [
@@ -125,19 +125,19 @@ const kristynsCloset = [
   ];
 
 ////// Kristyn's left shoe has traveled through time and space and turned up in Thom's accessories drawer! Remove Kristyn's shoe from the array and save it to the variable kristynsShoe. Use that variable to add Kristyn's lost shoe to Thom's accessories array.
-// const kristynShoe = kristynsCloset.shift();
-// console.log(kristynShoe);
+const kristynShoe = kristynsCloset.shift();
+console.log(kristynShoe);
 
-// thomsCloset[2].push(kristynShoe);
-// console.log(thomsCloset);
+thomsCloset[2].push(kristynShoe);
+console.log(thomsCloset);
 
 ////// Modify your code to put together 3 separate outfits for Kristyn and Thom. Put the output in a sentence to tell us what we'll be wearing. Mix and match!
 
 
-// console.log(`Kristyn's outfits this week are: (1.) ${kristynsCloset[3]} and ${kristynsCloset[2]} with ${kristynsCloset[0]}, (2.) ${kristynsCloset[1]} and ${kristynsCloset[4]} with ${kristynsCloset[5]}, and (3.) ${kristynsCloset[0]} and ${kristynsCloset[2]} with ${kristynsCloset[3]}.`)
+console.log(`Kristyn's outfits this week are: (1.) ${kristynsCloset[3]} and ${kristynsCloset[2]} with ${kristynsCloset[0]}, (2.) ${kristynsCloset[1]} and ${kristynsCloset[4]} with ${kristynsCloset[5]}, and (3.) ${kristynsCloset[0]} and ${kristynsCloset[2]} with ${kristynsCloset[3]}.`)
 
 
-// console.log(`Thom's outfits this week are: (1.) ${thomsCloset[0][1]} and ${thomsCloset[2][2]} with ${thomsCloset[0][2]}, (2.) ${thomsCloset[1][1]} and ${thomsCloset[0][0]} with ${thomsCloset[2][0]}, and (3.) ${thomsCloset[1][2]} and ${thomsCloset[0][1]} with ${thomsCloset[2][2]}.`)
+console.log(`Thom's outfits this week are: (1.) ${thomsCloset[0][1]} and ${thomsCloset[2][2]} with ${thomsCloset[0][2]}, (2.) ${thomsCloset[1][1]} and ${thomsCloset[0][0]} with ${thomsCloset[2][0]}, and (3.) ${thomsCloset[1][2]} and ${thomsCloset[0][1]} with ${thomsCloset[2][2]}.`)
 
 //// Time to do laundry - loop through Kristyn's closet and log the sentence "WHIRR: Now washing (item)" for each item in the array.
 
@@ -148,33 +148,17 @@ for (let i = 0; i < kristynsCloset.length; i++) {
 
 // //// Thom wants to do inventory on his closet. Using bracket notation, log the arrays (not the elements in the arrays) containing all of Thom's shirts, pants, and accessories.
 
-// for (let i = 0; i <= thomsCloset.length; i++) {
-//   console.log(thomsCloset[i])
-// }
+for (let i = 0; i <= thomsCloset.length; i++) {
+  console.log(thomsCloset[i])
+}
 
 
 //////Find the sum of all the multiples of 3 or 5 below 1000
 
-// let sum = 0;
-// for (let i = 0; i < 1000; i++) {
-//     if (i % 3 === 0 || i % 5 === 0) {
-//        sum += i;
-//     }
-// }
-// console.log(sum); //233168
-
-
-// const kitty = 'Love me, pet me! HSSSSSS!';
-// const kittyArray = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
-
-
-// for (let i = 1; i <= 20; i++) {
-//     if (i % 2 === 1) {
-//     console.log(kitty);
-//     } else {
-//         const randomIndex = Math.floor(Math.random() * 3)
-//     console.log(kittyArray[randomIndex]);
-//     }
-// }
-
-console.log(Math.floor(Math.random() * 3));
+let sum = 0;
+for (let i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+       sum += i;
+    }
+}
+console.log(sum); //233168
